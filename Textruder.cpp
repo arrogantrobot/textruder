942caab4e87f7f2a950d99d7968533a9013b0bfa
arrogantrobot <arrogantrobot@gmail.com>
2013-01-09 02:08:26 -0600

    use nanosleep instead of usleep

--- Textruder.cpp
+++ Textruder.cpp
@@ -1,38 +1,54 @@
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "Textruder.h"
 
-Textruder::Textruder(std::istream *in, std::ostream *out, int width) {
+using namespace textruder;
+
+Textruder::Textruder(std::istream *in, std::ostream *out, int width,
+        int rows_per_second) {
     input = in;
     output = out;
     endOfInput = false;
-    usec_per_frame = 35000;
-    nozzle = new CellularAutomaton(width);
-}
-
-void Textruder::run() {
-    struct timespec ts1, ts2;
-    long usec = 0;
-    while (!endOfInput) {
-        clock_gettime(CLOCK_REALTIME, &ts1);
-        printRow(nozzle->getRow());
-        clock_gettime(CLOCK_REALTIME, &ts2);
-        usec = (ts2.tv_nsec - ts1.tv_nsec)/1000;
-        if (usec < 0 || usec > usec_per_frame) {
-            usec = 0;
-        }
-        usleep(usec_per_frame - usec);
-    }
-}
-
-void Textruder::setDelayMSec(int delay) {
-    usec_per_frame = delay;
+    usec_per_frame = MICROSECONDS_PER_SECOND / rows_per_second;
+    nozzle = new ca::CellularAutomaton(width);
 }
 
 Textruder::~Textruder() {
-    if (feeder != NULL)
-        delete feeder;
     if (nozzle != NULL)
         delete nozzle;
 } 
+
+void Textruder::run() {
+    struct timespec ts1, ts2, sleepTime, remainingSleep;
+    sleepTime.tv_sec = remainingSleep.tv_nsec = 0;
+    remainingSleep.tv_sec = 0;
+    long usec = 0;
+
+    //loop until EOF is read from input
+    while (!endOfInput) {
+
+        //get time before print row
+        clock_gettime(CLOCK_REALTIME, &ts1);
+        printRow(nozzle->getRow());
+        //get time after print row
+        clock_gettime(CLOCK_REALTIME, &ts2);
+
+        //wait for usec_per_frame - time spent in printRow
+        usec = (ts2.tv_nsec - ts1.tv_nsec) / NANOSECONDS_PER_MICROSECOND;
+        if (usec < 0 || usec > usec_per_frame) {
+            usec = 0;
+            printf("ran out of time!\n");
+        }
+        long waitTime = (usec_per_frame - usec) * 1000;
+        sleepTime.tv_nsec = waitTime;
+        while(nanosleep(&sleepTime, &sleepTime) == -1) {}
+    }
+}
+
+void Textruder::setDelayUSec(int delay) {
+    usec_per_frame = delay;
+}
 
 void Textruder::printRow(const std::vector<int>& row) {
     int size = row.size();
@@ -44,7 +60,7 @@ void Textruder::printRow(const std::vector<int>& row) {
 }   
 
 char Textruder::getChar() {
-    char answer;
+    char answer = ' ';
     bool found = false;
     do {
         if (!input->eof()) {
@@ -57,8 +73,9 @@ char Textruder::getChar() {
                             break;
             }
         } else {
-
+            found = endOfInput = true;
         }
     } while (!found);
+
     return answer; 
 }  
--- Textruder.h
+++ Textruder.h
@@ -1,26 +1,67 @@
 #pragma once
 
-#include <sys/time.h>
 #include <iostream>
-#include "Feeder/Feeder.h"
-#include "Nozzle/Nozzle.h"
 #include "Nozzle/CellularAutomaton.h"
 
-class Textruder {
-public:
-    Textruder(std::istream *in, std::ostream *out, int width);
-    ~Textruder();
-    void run();
-    void setDelayMSec(int delay);
+namespace textruder {
 
-private:
-    std::istream *input;
-    std::ostream *output;
-    int usec_per_frame;
-    bool endOfInput;
-    Feeder *feeder;
-    Nozzle *nozzle;
-    char getChar();
-    void printRow(const std::vector<int>& row);
-    void initialize(int width);
-};
+#define NANOSECONDS_PER_MICROSECOND 1000
+#define MICROSECONDS_PER_SECOND 1000000
+
+    /**
+     * Textruder class, coordinate the feed
+     * and nozzle, regulate flow rate
+     */
+    class Textruder {
+    public:
+
+        /**
+         * @param in istream, source of text to extrude
+         * @param out ostream, destination of the extruded result
+         * @param width int, width in characters
+         */
+        Textruder(std::istream *in, std::ostream *out, int width, 
+                int _row_per_second);
+
+        /**
+         * Destructor
+         */
+        ~Textruder();
+
+        /**
+         * Once the components are ready, launch the
+         * output by calling run()
+         */
+        void run();
+
+        /** 
+         * Time between outputing rows, in milliseconds
+         */
+        void setDelayUSec(int delay);
+
+    private:
+
+        /**
+         * Neither the input nor the output streams
+         * will be closed or deallocated
+         */
+        std::istream *input;
+        std::ostream *output;
+
+        long usec_per_frame;
+        bool endOfInput;
+
+        /**
+         * Nozzle deallocated by the destructor
+         */
+        Nozzle *nozzle;
+
+        /**
+         * Return a single character from the input
+         * Will not be a tab or newline
+         */
+        char getChar();
+        void printRow(const std::vector<int>& row);
+        void initialize(int width);
+    };
+}// namespace textruder
