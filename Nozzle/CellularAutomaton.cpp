d2ac0abb996b913cacc29c12537876987ff44d6a
arrogantrobot <arrogantrobot@gmail.com>
2013-01-09 02:09:15 -0600

    Minor changes for clarity and speed

--- Nozzle/CellularAutomaton.cpp
+++ Nozzle/CellularAutomaton.cpp
@@ -1,9 +1,20 @@
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
 #include "CellularAutomaton.h"
+
+using namespace ca;
 
 void CellularAutomaton::initialize() {
     ruleTime = DEFAULT_RULE_TIME;
     lineCount = 0;
-    int rules [] = { 57,18,90,129,130,131,132,133 };
+    middleCell = width / 2;
+    lastCell = width - 1;
+
+    //initialize rule list with aestheticlly pleasing rules
+    int rules [] = { 30,57,18,90,129,130,131,132,133 };
     for (int i = 0; i < NUM_RULES; i++)
         ruleBank.push_back(rules[i]);
     rule = DEFAULT_RULE;
@@ -22,16 +33,15 @@ void CellularAutomaton::populateMask() {
 
 void CellularAutomaton::populateCells() {
     for (int i = 0; i < width; i++) {
-        cells.push_back(0);
-        newCells.push_back(0);
+        cells.push_back(CELL_OFF);
+        newCells.push_back(CELL_OFF);
     }
 }
 
 void CellularAutomaton::resetCells() {
     srand((unsigned)time(0) + (unsigned)getpid());
     if (rand() % 2) {
-        int mid = width / 2;
-        cells[mid] = CELL_ON;
+        cells[middleCell] = CELL_ON;
     } else {
         for (int i = 0; i < width; i++)
             if (rand() % 2) cells[i] = CELL_ON;
@@ -40,37 +50,37 @@ void CellularAutomaton::resetCells() {
 
 int CellularAutomaton::getCell(int i) {
     int idx = 0;
+
+    //lower cell
     if (i == 0) {
-        if (cells[width - 1] == CELL_ON)
-            idx += 1;
+        if (cells[lastCell] == CELL_ON)
+            idx |= 1;
     } else {
         if (cells[(i - 1) % width] == CELL_ON) {
-            idx += 1;
+            idx |= 1;
         }
     }
+
+    //same cell
     if (cells[i % width] == CELL_ON) {
-        idx += 2;
+        idx |= 2;
     }
-    if (i == (width - 1)) {
+
+    //higher cell
+    if (i == (lastCell)) {
         if (cells[0] == CELL_ON) {
-            idx += 4;
+            idx |= 4;
         }
     } else {
         if (cells[(i + 1) % width] == CELL_ON) {
-            idx += 4;
+            idx |= 4;
         }
     }
     return idx;
 }
 
 bool CellularAutomaton::resetRequired() {
-    int total = 0;
-    for (int i = 0; i < width; i++) {
-        if (cells[i] == CELL_ON) {
-            total++;
-        }
-    }
-    return total == width || total == 0;
+    return totalLiveCells == width || totalLiveCells == 0;
 }
 
 bool CellularAutomaton::ruleChangeRequired() {
@@ -107,9 +117,11 @@ void CellularAutomaton::calcNextRow() {
     for (int i = 0; i < width; i++) {
         newCells[i] = ((rule & mask[getCell(i)]) >= 1) ? CELL_ON : CELL_OFF;
     }
-
+    totalLiveCells = 0;
     for (int i = 0; i < width; i++) {
-        cells[i] = newCells[i];
+        //assign new cell value AND count living cells
+        if((cells[i] = newCells[i]) == CELL_ON)
+            totalLiveCells++;
     }
     lineCount++;
 }
--- Nozzle/CellularAutomaton.h
+++ Nozzle/CellularAutomaton.h
@@ -1,11 +1,9 @@
 #pragma once
 
 #include <vector>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 #include "Nozzle.h"
+
+namespace ca {
 
 #define NUM_RULES 8
 #define MIN_RULE_TIME 5
@@ -13,31 +11,35 @@
 #define DEFAULT_RULE_TIME 30
 #define DEFAULT_RULE 30
 
-enum {
-    CELL_OFF = 0,
-    CELL_ON
-};
+    enum {
+        CELL_OFF = 0,
+        CELL_ON
+    };
 
-class CellularAutomaton: public Nozzle {
-public:
-    CellularAutomaton(int wide) : Nozzle(wide) { initialize();};
-    ~CellularAutomaton();
-    void resetCells();
-    void calcNextRow();
-    const std::vector<int>& getRow();
-    int getCell(int index);
-    int getRule();
+    class CellularAutomaton: public Nozzle {
+    public:
+        CellularAutomaton(int wide) : Nozzle(wide) { initialize();};
+        ~CellularAutomaton();
 
-private:
-    int rule, defaultRules, lineCount, ruleTime;
-    std::vector<int> cells, newCells, mask, ruleBank;
+    private:
+        const std::vector<int>& getRow();
+        int getCell(int index);
+        int getRule();
+        int rule, defaultRules, lineCount, ruleTime;
+        int middleCell, lastCell;
+        int totalLiveCells;
+        std::vector<int> cells, newCells, mask, ruleBank;
 
-    void initialize();
-    void populateMask();
-    void populateCells();
-    void update();
-    void changeRule(int rule);
-    bool resetRequired();
-    bool ruleChangeRequired();
-    int getRandomRule();
-};
+        void resetCells();
+        void calcNextRow();
+        void initialize();
+        void populateMask();
+        void populateCells();
+        void update();
+        void changeRule(int rule);
+        bool resetRequired();
+        bool ruleChangeRequired();
+        int getRandomRule();
+    };
+
+} // ca namespace
